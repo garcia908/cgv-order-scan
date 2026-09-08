bc052686ab8b0d89fdaddb4dda45354ba0563fdb
jeon <jeonmarshallgarcia@gmail.com>
2026-09-08 13:55:23 +0700

    Refactor pinoHttp logger initialization and serializers

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -14,17 +14,17 @@ import {
 const app: Express = express();
 
 app.use(
-  pinoHttp({
+  (pinoHttp as any)({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
